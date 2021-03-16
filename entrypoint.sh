5a174083238f4441d2ada222a06f3a50e55207d3
Rachel D. Cartwright <Rachel.D.Cartwright@gmail.com>
2021-03-16 18:48:23 -0500

    get db to start up finally!

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,18 @@
 
 # Wait until Postgres is ready
 echo "running entrypoint.sh..."
-while ! [pg_isready -q -h $PGHOST -p $PGPORT -U $PGUSER]
+echo $PGHOST
+echo $PGPORT
+echo $PGUSER
+echo $PGDATABASE
+while ! pg_isready -q -h $PGHOST -p $PGPORT -U $PGUSER
 do
   echo "$(date) - waiting for database to start"
   sleep 2
 done
 
 # Create, migrate, and seed database if it doesn't exist.
-if [[ -z `psql -Atqc "\\list $PGDATABASE"` ]]
+if [ -z `psql -Atqc "\\list $PGDATABASE"` ]
 then
   echo "Database $PGDATABASE does not exist. Creating..."
   createdb -E UTF8 $PGDATABASE -l en_US.UTF-8 -T template0
