8f909a0f43b9e043d26afe23f40f313f9bb15977
Rachel D. Cartwright <stormchica@gmail.com>
2021-04-15 21:34:40 -0500

    add seeds

--- server/priv/repo/seeds.exs
+++ server/priv/repo/seeds.exs
@@ -9,3 +9,8 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+IO.puts("Adding a couple of users...")
+
+API.Account.create_user(%{email: "user1@email.com", password: "qwerty"})
+API.Account.create_user(%{email: "user2@email.com", password: "asdfgh"})
