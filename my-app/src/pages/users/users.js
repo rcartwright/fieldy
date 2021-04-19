f9e81fe0c942859c1be0e1c93f7045b5fc284cda
Rachel D. Cartwright <stormchica@gmail.com>
2021-04-18 21:50:26 -0500

    set up redux so that it successfully calls users from backend

--- my-app/src/pages/users/users.js
+++ my-app/src/pages/users/users.js
@@ -1,19 +1,40 @@
 //import("react-redux").DefaultRootState)
-import { fetchUsers, userSlice } from './../../features/users/userSlice';
+import React, { useEffect } from 'react'
+import {fetchUsers, userSlice, getUsers } from './../../features/users/userSlice';
 import { useSelector, useDispatch } from 'react-redux';
 
 export function Users() {
     const dispatch = useDispatch();
-    const users = dispatch(fetchUsers());
-    //const users = useSelector(state => state.users)
+    const userStatus = useSelector(state => state.users.status)
+    const users = useSelector(state => state.users.users)
+   // const users = useSelector(getUsers);
+    console.log('users', users);
 
-    function showUsers(users) {
-        return users?.map((user) => {
-            return user.email;
-        })
+    useEffect(() => {
+        if (userStatus === 'idle') {
+            dispatch(fetchUsers())
+        }
+    }, [userStatus, dispatch])
+
+    let content;
+
+    if (userStatus === 'loading') {
+        content = <div>Loading...</div>;
+    } else if (userStatus === 'success') {
+        console.log('users when success', users)
+        function showUsers(users) {
+            return users?.map((user) => {
+                return user.email;
+            })
+        }
+
+        content = <div>{showUsers(users)}</div>;
+    } else if (userStatus === 'error') {
+        content = <div>error</div>;
     }
 
-    return users && (
-        <div>{showUsers(users)}</div>
-    )
+    return <div>{content}</div>
+
+
+
 }
