5d283e0c97fb3066aaaa3af0cf6c242bf20e99b8
Rachel D. Cartwright <stormchica@gmail.com>
2021-04-19 00:52:08 -0500

    show users in a pretty table from server

--- my-app/src/pages/users/users.js
+++ my-app/src/pages/users/users.js
@@ -3,6 +3,67 @@ import {fetchUsers, userSlice } from './../../features/users/userSlice';
 import { useSelector, useDispatch } from 'react-redux';
 import Layout from '../../components/layout.js'
 
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+export default function DenseTable({users}) {
+  const classes = useStyles();
+  console.log('users', users)
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell align="right">Active?</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                Rachel
+              </TableCell>
+              <TableCell>{row.email}</TableCell>
+              <TableCell align="right">{row.is_active}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+function UsersList(props) {
+    
+}
+
 export function Users() {
     const dispatch = useDispatch();
     const userData = useSelector(state => state.userState)
@@ -13,18 +74,18 @@ export function Users() {
         }
     }, [userData.status, dispatch])
 
-    function showUsers(users) {
-        return users?.map((user) => {
-            return user.email;
-        })
-    }
+    // function showUsers(users) {
+    //     return users?.map((user) => {
+    //         return user.email;
+    //     })
+    // }
 
     let content;
     if (userData.status === 'loading') {
         content = <div>Loading...</div>;
     } else if (userData.status === 'success') {
         console.log('userData.users', userData.users)
-        content = <div>{showUsers(userData.users)}</div>;
+        content = <DenseTable users={userData.users} />;
     } else if (userData.status === 'error') {
         content = <div>{userData.error}</div>;
     }
