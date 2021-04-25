577c1161f252f41ef1e48998066d31c5cbbc030e
Rachel D. Cartwright <stormchica@gmail.com>
2021-04-25 16:21:23 -0500

    start designing show page

--- my-app/src/pages/organizations/show.js
+++ my-app/src/pages/organizations/show.js
@@ -5,15 +5,41 @@ import {
   Link,
   useParams
 } from "react-router-dom";
+import Title from '../../components/title';
 import React, { useEffect, useState } from 'react'
 import { fetchOrganizations } from './../../features/organizations/organizationSlice';
 import { useSelector, useDispatch } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Layout from '../../components/layout.js';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+
+const useStyles = makeStyles((theme) => ({
+  table: {
+    minWidth: 650,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+  tableLink: {
+    cursor: 'pointer'
+  }
+}));
 
 export const ShowOrganization = () => {
+    const classes = useStyles();
     const dispatch = useDispatch();
     let { id } = useParams();
     const orgData = useSelector(state => state.organizationState)
-    console.log('orgData', orgData)
 
     useEffect(() => {
       if (orgData.status === 'idle') {
@@ -22,12 +48,19 @@ export const ShowOrganization = () => {
     }, [orgData.status, dispatch])
 
     const org = orgData.organizations.find((org) => org.id == id);
-    console.log('org', org)
 
     if (orgData.status === 'loading' || orgData.status === 'idle') {
         return <div style={{padding: '30px'}}>Loading...</div>;
     }
     return (
-        <div>Show Org: {id} {org.name}</div>
+        <Layout>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Paper className={classes.paper}>
+                <Title>{org.name}</Title>
+              </Paper>
+            </Grid>
+          </Grid>
+        </Layout>
     )
 }
