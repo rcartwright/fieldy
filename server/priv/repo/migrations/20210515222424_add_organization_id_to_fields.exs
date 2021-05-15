b68dbbe43caf91c0b9f83a060e94c3a35167741e
Rachel D. Cartwright <stormchica@gmail.com>
2021-05-15 18:06:17 -0500

    add foreign key

--- server/priv/repo/migrations/20210515222424_add_organization_id_to_fields.exs
+++ server/priv/repo/migrations/20210515222424_add_organization_id_to_fields.exs
@@ -3,6 +3,7 @@ defmodule API.Repo.Migrations.AddOrganizationIdToFields do
 
   def change do
     alter table(:fields) do
+      add :organization_id, references(:organizations, type: :binary_id, column: :id)
     end
   end
 end
